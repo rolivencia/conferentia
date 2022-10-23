35d97dae53b8e10c733522d06b91f377027df13d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-10-23 18:02:10 -0300

    [I-1] Added Sanity connector service. Registered in app.module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { EventController } from '../event/event.controller';
 import { EventService } from '../event/event.service';
+import { ConnectorService } from '../shared/connectors/connector.service';
+import { SanityConnector } from '../shared/connectors/sanity.connector';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController, EventController],
-  providers: [AppService, EventService],
+  providers: [
+    AppService,
+    EventService,
+    { provide: ConnectorService, useClass: SanityConnector },
+  ],
 })
 export class AppModule {}
