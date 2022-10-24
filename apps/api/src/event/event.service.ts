066de41adb1295d83686b3ca35a273c0b23450a2
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-10-24 08:13:23 -0300

    [BE-11] Modified queries and added types to the queries results

--- apps/api/src/event/event.service.ts
+++ apps/api/src/event/event.service.ts
@@ -2,21 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { IEvent } from '@conferentia/models';
 import { ConnectorService } from '../shared/connectors/connector.service';
 
-// ToDo: Get rid of this array once the connection with Sanity CMS is made (INF - #1)
-const events = [
-  {
-    id: 1,
-    name: 'Conf',
-    activities: [
-      { id: 1, title: 'Acto de Apertura' },
-      { id: 2, title: 'Primera Charla' },
-      { id: 3, title: 'Coffee Break' },
-      { id: 4, title: 'Charla Magistral' },
-      { id: 5, title: 'Acto de Cierre' },
-    ],
-  },
-];
-
 @Injectable()
 export class EventService {
   constructor(private connectorService: ConnectorService) {}
@@ -24,11 +9,12 @@ export class EventService {
   public async get(id: number): Promise<IEvent> {
     const query = `*[_type == 'event' && _id == '${id}']`;
     const result = await this.connectorService.connector.fetch(query, {});
-    return result.length > 0 ? result.pop() : null;
+    return result.length > 0 ? (result.pop() as IEvent) : null;
   }
 
   public async getAll(): Promise<IEvent[]> {
-    const query = await this.connectorService.connector.fetch(`*`, {});
-    return query as IEvent[];
+    const query = `*[_type == 'event']`;
+    const result = await this.connectorService.connector.fetch(query, {});
+    return result as IEvent[];
   }
 }
