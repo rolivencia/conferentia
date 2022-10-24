61aaeee953ea6717cbc76f0e2ec1e44fe0ffdb1b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-10-24 11:06:07 -0300

    [DM-14] Added validation rules to Activity and Event schema

--- apps/studio/schemas/activity.ts
+++ apps/studio/schemas/activity.ts
@@ -8,14 +8,36 @@ export default {
       title: 'Evento',
       name: 'event',
       type: 'reference',
-      description: 'Evento al que pertenece la actividad',
+      description: 'Evento al que pertenece la actividad.',
       to: [{ type: 'event' }],
+      validation: (Rule) => Rule.required(),
     },
     {
       title: 'Título',
       name: 'title',
       type: 'string',
       description: 'Título asignado a la actividad.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Tipo de Actividad',
+      name: 'activity_type',
+      description: 'Tipo asignado a la actividad.',
+      type: 'reference',
+      to: [{ type: 'activity_type' }],
+      // options: {
+      //   filter: ({ document }) => {
+      //     // TODO: Put this filter to work (see #15)
+      //     // Check that the assigned Activity Type belongs to the same Event the activity belongs to
+      //     return {
+      //       filter: 'event == $event',
+      //       params: {
+      //         event: document.event,
+      //       },
+      //     };
+      //   },
+      // },
+      validation: (Rule) => Rule.required(),
     },
   ],
 };
--- apps/studio/schemas/event.ts
+++ apps/studio/schemas/event.ts
@@ -9,6 +9,7 @@ export default {
       type: 'string',
       description:
         'Nombre asignado a cada uno de los eventos gestionados por la plataforma.',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'image',
@@ -34,6 +35,7 @@ export default {
         dateFormat: 'YYYY-MM-DD',
         calendarTodayLabel: 'Hoy',
       },
+      validation: (Rule) => Rule.required(),
     },
     {
       title: 'Fecha de Finalización',
@@ -44,6 +46,11 @@ export default {
         dateFormat: 'YYYY-MM-DD',
         calendarTodayLabel: 'Hoy',
       },
+      validation: (Rule) => Rule.required(),
     },
   ],
+  initialValue: {
+    start_date: new Date(),
+    end_date: new Date()
+  }
 };
