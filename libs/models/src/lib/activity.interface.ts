0330619bd2d900449626e1800389b71fb5cbb464
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-10-25 11:04:46 -0300

    [DM-14] Updated Activity model

--- libs/models/src/lib/activity.interface.ts
+++ libs/models/src/lib/activity.interface.ts
@@ -1,8 +1,15 @@
 import { IAudit } from './audit.interface';
 import { IEvent } from './event.interface';
+import { IParticipant } from './participant.interface';
+import {IActivityType} from "./activity-type.interface";
 
 export interface IActivity extends IAudit {
   id: string;
   title: string;
   event: IEvent;
+  description: string[];
+  participants: IParticipant[];
+  startDate: Date | string;
+  endDate: Date | string;
+  type: IActivityType;
 }
