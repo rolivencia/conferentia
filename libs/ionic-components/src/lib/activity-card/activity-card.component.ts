a50e37a3c819255d933b6f3e4bfe45121fa4b98b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-03 21:25:08 -0300

    [FE-#35] Added stubs for ActivityCard and SubjectAreaCard components

---
+++ libs/ionic-components/src/lib/activity-card/activity-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'conferentia-activity-card',
+  templateUrl: './activity-card.component.html',
+  styleUrls: ['./activity-card.component.scss'],
+})
+export class ActivityCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/ionic-components/src/lib/subject-area/subject-area.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'conferentia-subject-area',
+  templateUrl: './subject-area.component.html',
+  styleUrls: ['./subject-area.component.scss'],
+})
+export class SubjectAreaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
