55ba41b6ecfe18d39d9bcf8a832ca8d4c5aefee0
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-04 11:00:21 -0300

    [FE-#38] Loaded event data from environment file

--- apps/app/src/_services/event.service.ts
+++ apps/app/src/_services/event.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IEvent } from '@conferentia/models';
-import { environment } from '../environments/environment';
-import { HttpService } from './http.service';
+import { BehaviorSubject, Observable, of, switchMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { HttpService } from './http.service';
+import { IEvent } from '@conferentia/models';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +11,26 @@ import { HttpClient } from '@angular/common/http';
 export class EventService extends HttpService {
   protected prefix = `${environment.apiUrl}/event`;
 
+  private currentEvent$: BehaviorSubject<IEvent | null> =
+    new BehaviorSubject<IEvent | null>(null);
+
   constructor(protected override http: HttpClient) {
     super(http);
   }
 
-  public get(id: number): Observable<IEvent> {
+  public get(id: number | string): Observable<IEvent> {
     return this.http.get<IEvent>(`${this.prefix}/${id}`);
+  }
+
+  // Used to set the current event at app boostrapping by
+  // reading the app environment.ts file
+  // TODO: Load event data based on SaaS-oriented configuration (2022/11/04 - RO - #40)
+  public setFromEnvironment(): Observable<IEvent> {
+    return this.get(environment.eventId).pipe(
+      switchMap((event) => {
+        this.currentEvent$.next(event);
+        return of(event);
+      })
+    );
   }
 }
--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, FactoryProvider, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,18 +9,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EventService } from '../_services/event.service';
 
+// TODO: Load event data based on SaaS-oriented configuration (2022/11/04 - RO - #40)
+export const loadCurrentEvent: FactoryProvider = {
+  provide: APP_INITIALIZER,
+  useFactory: loadEventFactory,
+  deps: [EventService],
+  multi: true,
+};
+
+function loadEventFactory(eventService: EventService) {
+  return () => eventService.setFromEnvironment();
+}
+
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        AppRoutingModule,
-        HttpClientModule,
-    ],
-    providers: [
-        EventService,
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    EventService,
+    // TODO: Load event data based on SaaS-oriented configuration (2022/11/04 - RO - #40)
+    loadCurrentEvent,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
