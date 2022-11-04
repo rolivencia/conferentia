a6cd4b0fadbf978d026b8fc37748c583f3821a77
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-03 21:25:34 -0300

    [FE-#35] Added naming to existing projects in project.json

--- apps/landing-unl-seminar-v1/src/app/folder/folder.module.ts
+++ apps/landing-unl-seminar-v1/src/app/folder/folder.module.ts
@@ -7,9 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { IonicComponentsModule } from "@conferentia/ionic-components";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule, IonicComponentsModule],
   declarations: [FolderPage],
 })
 export class FolderPageModule {}
