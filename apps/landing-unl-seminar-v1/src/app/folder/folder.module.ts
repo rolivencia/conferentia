27aff28af2251518a8d395eb19356a1f9ae99d9a
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-03 21:27:24 -0300

    [FE-#35] Rolled back changes that were for testing library components

--- apps/landing-unl-seminar-v1/src/app/folder/folder.module.ts
+++ apps/landing-unl-seminar-v1/src/app/folder/folder.module.ts
@@ -7,10 +7,9 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
-import { IonicComponentsModule } from "@conferentia/ionic-components";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule, IonicComponentsModule],
+  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
   declarations: [FolderPage],
 })
 export class FolderPageModule {}
