0417131599ad5c9eccedbea409aac49c6518ce83
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-08 10:03:29 -0300

    [FE-#49] Updated prod environment file.

--- apps/landing-unl-seminar-v1/src/environments/environment.prod.ts
+++ apps/landing-unl-seminar-v1/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { IFrontendEnvironmentConfig } from '@conferentia/models';
 export const environment: IFrontendEnvironmentConfig = {
   production: true,
   //TODO: Move these configuration to .env file and consume it from there (2022/11/04 - RO - #39)
-  apiUrl: '',
+  apiUrl: 'https://conferentia.onrender.com/api',
   eventId: 'ca40fed4-177c-4a9f-bcfa-225c2b22419d',
 };
