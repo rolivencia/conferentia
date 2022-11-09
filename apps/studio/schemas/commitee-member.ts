8a2ce13502af68b585bd034939245d3c6906bbbb
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-09 00:02:02 -0300

    [CO-#51] Added Sanity schemas for CommitteeMember and CommitteeArea entities

---
+++ apps/studio/schemas/commitee-member.ts
@@ -0,0 +1,64 @@
+export default {
+  title: 'Miembros del Comité',
+  name: 'committeeMember',
+  type: 'document',
+  description: 'Miembros del comité de un evento',
+  preview: {
+    select: { firstName: 'firstName', lastName: 'lastName', courtesyName: 'courtesyName'},
+    prepare(selection) {
+      const {firstName, lastName, courtesyName } = selection;
+      const courtesyString = courtesyName ? `, ${courtesyName}.` : '';
+      return {
+        title: `${firstName} ${lastName}${courtesyString}`,
+      }
+    }
+  },
+  fields: [
+    {
+      title: 'Evento',
+      name: 'event',
+      type: 'reference',
+      description: 'Evento al que pertenece el miembro del comité.',
+      to: [{ type: 'event' }],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Nombre de Cortesía',
+      name: 'courtesyName',
+      type: 'string',
+      description: 'Dr., PhD., Ms, etc.',
+    },
+    {
+      title: 'Nombre',
+      name: 'firstName',
+      type: 'string',
+      description: 'Nombre del miembro del comité.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Apellido',
+      name: 'lastName',
+      type: 'string',
+      description: 'Apellido del miembro del comité.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Institución',
+      name: 'institution',
+      type: 'string',
+      description: 'Institución a la que pertenece el miembro del comité.',
+    },
+    {
+      title: 'Avatar',
+      name: 'avatar',
+      type: 'image',
+      description: 'Imagen de la persona para presentar en la aplicación.',
+    },
+    {
+      title: 'Currículum',
+      name: 'curriculum',
+      type: 'text',
+      description: 'Currículum del miembro del comité.',
+    },
+  ]
+}
---
+++ apps/studio/schemas/committee-area.ts
@@ -0,0 +1,59 @@
+export default {
+  title: 'Áreas de Comité Académico',
+  name: 'committeeArea',
+  type: 'document',
+  description: 'Áreas de los Comités Académicos de un evento',
+  fields: [
+    {
+      title: 'Evento',
+      name: 'event',
+      type: 'reference',
+      description: 'Evento al que pertenece el comité.',
+      to: [{ type: 'event' }],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Nombre',
+      name: 'name',
+      type: 'string',
+      description: 'Nombre asignado al área.',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Chairs',
+      name: 'chairs',
+      type: 'array',
+      description: 'Chair(s) del comité',
+      of: [
+        {
+          type: 'reference',
+          to: { type: 'committeeMember' },
+        },
+      ],
+    },
+    {
+      title: 'Vice Chairs',
+      name: 'viceChairs',
+      type: 'array',
+      description: 'Vice-Chair(s) del comité',
+      of: [
+        {
+          type: 'reference',
+          to: { type: 'committeeMember' },
+        },
+      ],
+    },
+    {
+      title: 'Integrantes',
+      name: 'members',
+      type: 'array',
+      description: 'Integrantes del comité',
+      of: [
+        {
+          type: 'reference',
+          to: { type: 'committeeMember' },
+        },
+      ],
+    },
+  ],
+};
