fe88b05a45d16cc9122a29dfb391ad2ca67bee39
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-09 00:57:36 -0300

    [CO-#51] Add CommitteeService to angular-services library

--- libs/angular-services/src/index.ts
+++ libs/angular-services/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/angular-services.module';
 
 // Services
+export { CommitteeService } from './lib/committee.service';
 export { EventService } from './lib/event.service';
 export { NavigationService } from './lib/navigation.service';
