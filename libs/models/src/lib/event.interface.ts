db04443c6a5f7cec0688af8e8097947e186b6cb3
Ramiro Olivencia <ramiro.olivencia@soteicavisualmesa.com>
2022-11-12 20:20:50 -0300

    [DM-#56] Added Image model and featuredImage property in Event model

--- libs/models/src/lib/event.interface.ts
+++ libs/models/src/lib/event.interface.ts
@@ -1,12 +1,14 @@
 import { IAudit } from './audit.interface';
 import { ISubjectArea } from './subject-area.interface';
-import { IEventSponsor } from "./event-sponsor.interface";
+import { IEventSponsor } from './event-sponsor.interface';
+import { IImage } from './image.interface';
 
 export interface IEvent extends IAudit {
   title: string;
   name: string;
   startDate: string | Date;
   endDate: string | Date;
+  featuredImage?: IImage;
   logo?: string;
   sponsors?: IEventSponsor[];
   subjectAreas?: ISubjectArea[];
---
+++ libs/models/src/lib/image.interface.ts
@@ -0,0 +1,4 @@
+export interface IImage {
+  url: string;
+  alt: string; // Alternative/reference text to show in relation to the image
+}
