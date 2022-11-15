ea5dd31832caa594b21659cda58cabb742172e5e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-15 11:20:34 -0300

    [AR-#58] Add missing return type in committee service

--- apps/api/src/committee/committee.service.ts
+++ apps/api/src/committee/committee.service.ts
@@ -6,12 +6,12 @@ import { ICommitteeArea } from '@conferentia/models';
 export class CommitteeService {
   constructor(private connectorService: ConnectorService) {}
 
-  public async getAreas(eventId: number | string) {
+  public async getAreas(eventId: number | string): Promise<ICommitteeArea[]> {
     const query = `*[_type == 'committeeArea' && references('${eventId}')]
                     {_id, _createdAt, _updatedAt, _type, _rev, name, chairs[]->, viceChairs[]->, members[]->, order } | order(order)`;
     const result = await this.connectorService.connector.fetch(query, {});
     return result.map((x) => {
-      const {order, ...entity} = x;
+      const { order, ...entity } = x;
       return entity;
     }) as ICommitteeArea[];
   }
