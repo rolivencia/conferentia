1a76d94eca16b4644469b08517b2687bac0a04e7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-15 11:30:37 -0300

    [AR-#58] Add Sortable interface

--- apps/api/src/committee/committee.service.ts
+++ apps/api/src/committee/committee.service.ts
@@ -1,6 +1,12 @@
+// Core
 import { Injectable } from '@nestjs/common';
-import { ConnectorService } from '../shared/connectors/connector.service';
+
+// Interfaces
 import { ICommitteeArea } from '@conferentia/models';
+import { Sortable } from '@conferentia/nest-modules';
+
+// Services
+import { ConnectorService } from '../shared/connectors/connector.service';
 
 @Injectable()
 export class CommitteeService {
@@ -10,7 +16,7 @@ export class CommitteeService {
     const query = `*[_type == 'committeeArea' && references('${eventId}')]
                     {_id, _createdAt, _updatedAt, _type, _rev, name, chairs[]->, viceChairs[]->, members[]->, order } | order(order)`;
     const result = await this.connectorService.connector.fetch(query, {});
-    return result.map((x) => {
+    return result.map((x: Sortable) => {
       const { order, ...entity } = x;
       return entity;
     }) as ICommitteeArea[];
