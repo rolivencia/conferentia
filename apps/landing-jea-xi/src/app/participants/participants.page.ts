0a0822bdfb09ecbb936e183ef81dc230d660f09e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-18 09:45:15 -0300

    [CO-#78] Added content in Participants page

--- apps/landing-jea-xi/src/app/participants/participants.page.ts
+++ apps/landing-jea-xi/src/app/participants/participants.page.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  EventService,
+  ParticipantService,
+} from '../../../../../libs/angular-services/src';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { IEvent, IParticipant } from '../../../../../libs/models/src';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'conferentia-participants',
   templateUrl: './participants.page.html',
   styleUrls: ['./participants.page.scss'],
 })
-export class ParticipantsPage implements OnInit {
-  constructor() {}
+export class ParticipantsPage {
+  public participants$: Observable<IParticipant[] | null> = of(null);
+  public currentEvent$: Observable<IEvent | null> = of(null);
 
-  ngOnInit() {}
+  constructor() {
+    const eventService = inject(EventService);
+    const participantService = inject(ParticipantService);
+    this.currentEvent$ = eventService.currentEvent$.asObservable();
+    this.participants$ = this.currentEvent$.pipe(
+      switchMap((event) =>
+        participantService.getByEventId((event as IEvent)._id)
+      )
+    );
+  }
 }
