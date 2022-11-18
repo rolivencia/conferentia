1d1f1711c5a89be50ef5c88785c90e051ec85f54
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-11-18 09:44:58 -0300

    [BE-#78] Fix property name in ParticipantService

--- apps/api/src/participant/participant.service.ts
+++ apps/api/src/participant/participant.service.ts
@@ -20,7 +20,7 @@ export class ParticipantService {
     // ToDo: #60 - Remove code duplication when fetching sorted domain entities, via use of the Sorting design pattern (RO - 2022/11/15)
     return result.map((participant) => ({
       ...participant,
-      image: builder.image(participant.avatar).url(),
+      avatar: builder.image(participant.avatar).url(),
     }));
   }
 }
