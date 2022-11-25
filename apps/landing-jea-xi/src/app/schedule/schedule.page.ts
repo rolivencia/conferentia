68ef59f3c1f3a529d3854301fde2eb45284120f7
Ramiro Olivencia <ramiro.olivencia@soteicavisualmesa.com>
2022-11-25 19:08:05 -0300

    [CO-#88] Change JEA XI app Schedule to use new component

--- apps/landing-jea-xi/src/app/schedule/schedule.page.ts
+++ apps/landing-jea-xi/src/app/schedule/schedule.page.ts
@@ -1,31 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { map, Observable, of, switchMap } from 'rxjs';
-import { IActivity, IEvent } from '@conferentia/models';
+import { Observable, of, switchMap } from 'rxjs';
+import { IEvent, Schedule } from '@conferentia/models';
 import { ActivityService, EventService } from '@conferentia/angular-services';
-import dayjs from 'dayjs'
 
 @Component({
-  selector: 'conferentia-schedule',
+  selector: 'conferentia-schedule-page',
   templateUrl: './schedule.page.html',
   styleUrls: ['./schedule.page.scss'],
 })
 export class SchedulePage {
-  public activities$: Observable<IActivity[] | null> = of(null);
+  public schedule$: Observable<Schedule | null> = of(null);
   public currentEvent$: Observable<IEvent | null> = of(null);
 
   constructor() {
     const eventService = inject(EventService);
     const activityService = inject(ActivityService);
     this.currentEvent$ = eventService.currentEvent$.asObservable();
-    this.activities$ = this.currentEvent$.pipe(
+    this.schedule$ = this.currentEvent$.pipe(
       switchMap((event) => activityService.getByEventId((event as IEvent)._id)),
-      map((activities) =>
-        activities.map((activity) => ({
-          ...activity,
-          startDate: dayjs(activity.startDate).format('HH:mm'),
-          endDate: dayjs(activity.endDate).format('HH:mm'),
-        }))
-      )
     );
   }
 }
