7f288ba2a243eeb392a7d223f47318a2b8398d29
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-05 11:53:23 -0300

    [DM-#96] Add additional properties to User model

--- libs/models/src/lib/user.interface.ts
+++ libs/models/src/lib/user.interface.ts
@@ -1,6 +1,11 @@
-import { IAudit } from '@conferentia/models';
+import { Country, IAudit } from '@conferentia/models';
 
 export interface User extends IAudit {
   email: string;
+  firstName: string;
+  lastName: string;
+  courtesyTitle: string;
+  affiliation: string;
+  country: Country;
   hasFinishedRegistration: boolean;
 }
