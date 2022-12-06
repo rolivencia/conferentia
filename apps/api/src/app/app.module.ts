89bd617b2a442b9435dfd69819a69eab9267f302
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-06 03:37:18 -0300

    [BE-#104] Sort dependencies. Add MulterModule for uploaded files management.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,31 +1,41 @@
+// Core
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '@nestjs/config';
-import { EventController } from '../event/event.controller';
-import { EventService } from '../event/event.service';
-import { ConnectorService } from '../shared/connectors/connector.service';
-import { SanityConnector } from '../shared/connectors/sanity.connector';
-import { CommitteeService } from '../committee/committee.service';
-import { CommitteeController } from '../committee/committee.controller';
-import { SubjectAreaController } from '../subject-area/subject-area.controller';
-import { SubjectAreaService } from '../subject-area/subject-area.service';
-import { SponsorService } from '../sponsor/sponsor.service';
-import { SponsorController } from '../sponsor/sponsor.controller';
-import { EntitySortingService } from '@conferentia/nest-modules';
-import { ActivityController } from '../activity/activity.controller';
-import { ActivityService } from '../activity/activity.service';
-import { ParticipantController } from '../participant/participant.controller';
-import { ParticipantService } from '../participant/participant.service';
-import { UserController } from '../user/user.controller';
-import { UserService } from '../user/user.service';
+// Controllers
 import { AbstractController } from '../abstract/abstract.controller';
+import { ActivityController } from '../activity/activity.controller';
+import { CommitteeController } from '../committee/committee.controller';
+import { EventController } from '../event/event.controller';
+import { ParticipantController } from '../participant/participant.controller';
+import { SanityConnector } from '../shared/connectors/sanity.connector';
+import { SponsorController } from '../sponsor/sponsor.controller';
+import { SubjectAreaController } from '../subject-area/subject-area.controller';
+import { UserController } from '../user/user.controller';
+
+// Modules
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+
+// Services
 import { AbstractService } from '../abstract/abstract.service';
+import { ActivityService } from '../activity/activity.service';
+import { CommitteeService } from '../committee/committee.service';
+import { ConnectorService } from '../shared/connectors/connector.service';
+import { EntitySortingService } from '@conferentia/nest-modules';
+import { EventService } from '../event/event.service';
+import { ParticipantService } from '../participant/participant.service';
+import { SponsorService } from '../sponsor/sponsor.service';
+import { SubjectAreaService } from '../subject-area/subject-area.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    MulterModule.register({
+      dest: './files',
     }),
   ],
   controllers: [
