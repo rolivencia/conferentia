6dcab58cfe8a4dc3cb0a551816afcb9dfffa79f3
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-06 03:32:38 -0300

    [DM-#104] Add format property to Abstract model. Add SubmittedAbstractPayload model.

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -10,7 +10,7 @@ export { ICommitteeMember } from './lib/committee-member.interface';
 export { ISubjectArea } from './lib/subject-area.interface';
 export { IEventSponsor } from './lib/event-sponsor.interface';
 
-export { Abstract } from './lib/abstract.interface';
+export { Abstract, SubmittedAbstractPayload } from './lib/abstract.interface';
 export { Author } from './lib/author.interface';
 export { Country, countries } from './lib/country.interface';
 export { Schedule } from './lib/schedule.interface';
--- libs/models/src/lib/abstract.interface.ts
+++ libs/models/src/lib/abstract.interface.ts
@@ -7,4 +7,24 @@ export interface Abstract extends IAudit {
   keywords: string;
   fileUrl: string;
   status: string;
+  format: string;
 }
+
+export interface SubmittedAbstractPayload {
+  abstract: {
+    title: string;
+    keywords: string;
+    subjectAreaId: string;
+    authors: AuthorPayload[];
+    file: File;
+    format: string;
+  };
+  eventId: string;
+  uploaderUserId: string;
+}
+
+export type AuthorPayload = {
+  firstName: string;
+  lastName: string;
+  institution: string;
+};
