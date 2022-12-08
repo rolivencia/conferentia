f503d6125ff69ad9a822742ed80d40486fd82ea7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-12-08 11:30:19 -0300

    [AR-#107] Read Auth0 config from environment file

--- apps/landing-unl-seminar-v1/src/environments/environment.ts
+++ apps/landing-unl-seminar-v1/src/environments/environment.ts
@@ -9,6 +9,11 @@ export const environment: IFrontendEnvironmentConfig = {
   //TODO: Move these configuration to .env file and consume it from there (2022/11/04 - RO - #39)
   apiUrl: 'http://localhost:3334/api',
   eventId: 'ca40fed4-177c-4a9f-bcfa-225c2b22419d',
+  auth0: {
+    domain: 'fawhc-2023.us.auth0.com',
+    clientId: '9wYZtEZPkHmS1yma7v892Ur6mojkmQ0C',
+    audience: 'https://fawhc-2023.us.auth0.com/api/v2/',
+  },
 };
 
 /*
