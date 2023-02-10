963eb09a19a9327d68ff381c4bebe32d4a22557e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-02-10 04:11:20 -0300

    [FE] Add basic Review Dashboard page grid

--- apps/landing-unl-seminar-v1/src/app/admin-dashboard/admin-dashboard.page.ts
+++ apps/landing-unl-seminar-v1/src/app/admin-dashboard/admin-dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractService, EventService } from '@conferentia/angular-services';
+import { Observable, of, switchMap } from 'rxjs';
+import { Abstract, IEvent } from '@conferentia/models';
 
 @Component({
   selector: 'conferentia-admin-dashboard',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.page.scss'],
 })
 export class AdminDashboardPage implements OnInit {
-  constructor() {}
+  public abstracts$: Observable<Abstract[]> = of([]);
+  public currentEvent$: Observable<IEvent | null> = of(null);
 
-  ngOnInit() {}
+  constructor(
+    private abstractService: AbstractService,
+    private eventService: EventService
+  ) {
+    this.currentEvent$ = eventService.currentEvent$;
+  }
+
+  ngOnInit() {
+    this.abstracts$ = this.currentEvent$.pipe(
+      switchMap((event) =>
+        this.abstractService.getByEventId((event as IEvent)._id)
+      )
+    );
+  }
 }
