1ee31944ce74f49580244074a302fd2f3eeac31e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-02-15 05:31:28 -0300

    [FE] Add colorStatusMap

---
+++ apps/landing-unl-seminar-v1/src/app/_providers/utils.ts
@@ -0,0 +1,9 @@
+export const colorStatusMap: { [key: string]: string } = {
+  submitted: 'primary',
+  inEvaluation: 'primary',
+  accepted: 'success',
+  acceptedWithModifications: 'tertiary',
+  rejected: 'danger',
+  assignedModalityOral: 'success',
+  assignedModalityFlashPoster: 'success',
+};
