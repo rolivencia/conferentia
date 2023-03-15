cd6fb5bc9195baba023ccfbea4b7a1520c14d8fd
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-15 02:25:35 -0300

    [CO] Update abstract submission deadlines

--- apps/landing-unl-seminar-v1/src/environments/environment.ts
+++ apps/landing-unl-seminar-v1/src/environments/environment.ts
@@ -1,11 +1,10 @@
-
 export const environment = {
-   production: false,
-   apiUrl: 'http://localhost:3334/api',
-   eventId: 'ca40fed4-177c-4a9f-bcfa-225c2b22419d',
-   auth0: {
-     domain: 'fawhc-2023.us.auth0.com',
-     clientId: '9wYZtEZPkHmS1yma7v892Ur6mojkmQ0C',
-     audience: 'https://fawhc-2023.us.auth0.com/api/v2/',
-   }
+  production: false,
+  apiUrl: 'http://localhost:3334/api',
+  eventId: 'ca40fed4-177c-4a9f-bcfa-225c2b22419d',
+  auth0: {
+    domain: 'dev-0wyaah0g.us.auth0.com',
+    clientId: 'JDyryGkwVp1H1uP3risPd0qo2c6il9NO',
+    audience: 'https://dev-0wyaah0g.us.auth0.com/api/v2/',
+  },
 };
