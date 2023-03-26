8a747cf679966135da5aa71313bceaecb9f24f6c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-26 17:21:58 -0300

    [FE] Add content for Abstract Revision page

--- apps/landing-unl-seminar-v1/src/app/submit-abstract-revision/submit-abstract-revision.module.ts
+++ apps/landing-unl-seminar-v1/src/app/submit-abstract-revision/submit-abstract-revision.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SubmitAbstractRevisionPageRoutingModule } from './submit-abstract-revision-routing.module';
 
 import { SubmitAbstractRevisionPage } from './submit-abstract-revision.page';
-import { IonicComponentsModule } from "@conferentia/ionic-components";
+import { IonicComponentsModule } from '@conferentia/ionic-components';
+import { AuthorsPipe } from '../_providers/authors.pipe';
 
 @NgModule({
   imports: [
@@ -15,8 +16,9 @@ import { IonicComponentsModule } from "@conferentia/ionic-components";
     FormsModule,
     IonicModule,
     SubmitAbstractRevisionPageRoutingModule,
-    IonicComponentsModule
+    IonicComponentsModule,
+    ReactiveFormsModule,
   ],
-  declarations: [SubmitAbstractRevisionPage],
+  declarations: [SubmitAbstractRevisionPage, AuthorsPipe],
 })
 export class SubmitAbstractRevisionPageModule {}
--- apps/landing-unl-seminar-v1/src/app/submit-abstract-revision/submit-abstract-revision.page.ts
+++ apps/landing-unl-seminar-v1/src/app/submit-abstract-revision/submit-abstract-revision.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EnvironmentInjector, inject, OnInit } from '@angular/core';
+import { first, Observable, of, switchMap, tap } from 'rxjs';
+import { Abstract } from '@conferentia/models';
+import { statusesList } from '../_providers/utils';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractService } from '@conferentia/angular-services';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'conferentia-submit-abstract-revision',
@@ -6,7 +13,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-abstract-revision.page.scss'],
 })
 export class SubmitAbstractRevisionPage implements OnInit {
-  constructor() {}
+  public abstract$: Observable<Abstract | null> = of(null);
 
-  ngOnInit() {}
+  public submitted = false;
+  public statuses = statusesList;
+
+  public form: FormGroup | undefined;
+
+  constructor(
+    private abstractService: AbstractService,
+    private formBuilder: FormBuilder,
+    private injector: EnvironmentInjector,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.abstract$ = this.route.queryParams.pipe(
+      first(),
+      switchMap((params) => this.abstractService.getById(params['abstractId'])),
+      tap((abstract) => this.load(abstract))
+    );
+  }
+
+  onDownloadClicked(href: string) {
+    window.open(href, '_blank');
+  }
+
+  onSaveClicked() {
+    this.submitted = true;
+    this.abstractService.review(this.form?.value).subscribe({
+      next: (abstract) => {
+        this.injector.runInContext(() => {
+          updateAlert();
+          this.submitted = false;
+        });
+      },
+      error: (error) => {
+        this.injector.runInContext(() => {
+          updateAlert(true);
+          this.submitted = false;
+        });
+      },
+    });
+  }
+
+  private load(abstract: Abstract) {
+    this.form = this.formBuilder.group({
+      _id: [abstract._id, Validators.required],
+      review: [abstract.review ?? '', Validators.required],
+      status: [abstract.status, Validators.required],
+    });
+  }
+}
+
+async function updateAlert(error = false) {
+  const alert = await inject(AlertController).create({
+    header: error ? 'Error' : 'Success',
+    cssClass: error ? 'error-alert' : 'success-alert',
+    message: error
+      ? 'There was a problem updating the Abstract review. Please try again'
+      : 'The abstract review updated successfully!',
+    buttons: ['Dismiss'],
+  });
+
+  await alert.present();
 }
