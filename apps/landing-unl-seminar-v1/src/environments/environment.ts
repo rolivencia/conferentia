9730e94933de7c26ec4443f1b254dcc4c191122e
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-03-27 17:32:47 +0200

    [FE] Add time zone to env variables and IFrontendEnvironmentConfig model

--- apps/landing-unl-seminar-v1/src/environments/environment.ts
+++ apps/landing-unl-seminar-v1/src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3334/api',
   eventId: 'ca40fed4-177c-4a9f-bcfa-225c2b22419d',
+  timeZone: -3,
   auth0: {
     domain: 'dev-0wyaah0g.us.auth0.com',
     clientId: 'JDyryGkwVp1H1uP3risPd0qo2c6il9NO',
