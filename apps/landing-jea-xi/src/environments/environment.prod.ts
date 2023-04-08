40e4d787eb5cc1e5c92e67da993dbf46a2a7a99f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-08 15:41:39 -0300

    [AR] Remove JEA XI app from workspace

--- apps/landing-jea-xi/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-import { IFrontendEnvironmentConfig } from '@conferentia/models';
-
-export const environment: IFrontendEnvironmentConfig = {
-  production: true,
-  //TODO: Move these configuration to .env file and consume it from there (2022/11/04 - RO - #39)
-  apiUrl: 'https://conferentia.onrender.com/api',
-  eventId: 'c848063b-a2d3-4817-b248-51bef7453fce',
-};
