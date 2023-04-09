7e4e6234e3441e6d3e2064c3666a2841cb234152
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-04-09 03:24:07 -0300

    [FE] Rearrange format of dates for Activity details

--- libs/angular-services/src/lib/services/activity.service.ts
+++ libs/angular-services/src/lib/services/activity.service.ts
@@ -25,43 +25,57 @@ export class ActivityService extends HttpService {
   }
 
   public getById(activityId: number | string): Observable<IActivity> {
-    return this.http.get<IActivity>(`${this.prefix}/getById/${activityId}`).pipe(
-      switchMap((activity) => {
-        return of({
-          ...activity,
-          startDate: formatActivitiesTime(
-            activity.startDate,
-            this.env.timeZone
-          ),
-          endDate: formatActivitiesTime(activity.endDate, this.env.timeZone),
-        });
-      })
-    );
+    return this.http
+      .get<IActivity>(`${this.prefix}/getById/${activityId}`)
+      .pipe(
+        switchMap((activity) => {
+          return of({
+            ...activity,
+            startDate: formatActivityDetailDateTime(
+              activity.startDate,
+              this.env.timeZone
+            ),
+            endDate: formatActivityDetailDateTime(
+              activity.endDate,
+              this.env.timeZone
+            ),
+          });
+        })
+      );
   }
 
   public getByEventId(eventId: number | string): Observable<Schedule> {
-    return this.http.get<Schedule>(`${this.prefix}/getByEventId/${eventId}`).pipe(
-      switchMap((schedule) => {
-        return of(
-          schedule.map((scheduleDay) => ({
-            ...scheduleDay,
-            activities: scheduleDay.activities.map((activity) => ({
-              ...activity,
-              startDate: formatActivitiesTime(
-                activity.startDate,
-                this.env.timeZone
-              ),
-              endDate: formatActivitiesTime(
-                activity.endDate,
-                this.env.timeZone
-              ),
-            })),
-          }))
-        );
-      })
-    );
+    return this.http
+      .get<Schedule>(`${this.prefix}/getByEventId/${eventId}`)
+      .pipe(
+        switchMap((schedule) => {
+          return of(
+            schedule.map((scheduleDay) => ({
+              ...scheduleDay,
+              activities: scheduleDay.activities.map((activity) => ({
+                ...activity,
+                startDate: formatActivitiesTime(
+                  activity.startDate,
+                  this.env.timeZone
+                ),
+                endDate: formatActivitiesTime(
+                  activity.endDate,
+                  this.env.timeZone
+                ),
+              })),
+            }))
+          );
+        })
+      );
   }
 }
 
-const formatActivitiesTime = (dateTime: string | Date, timeZone: number = 0) =>
-  dayjs(dateTime).utcOffset(timeZone).format('hh:mm');
+const formatActivitiesTime = (
+  dateTime: string | Date,
+  timeZone: number = 0
+): string => dayjs(dateTime).utcOffset(timeZone).format('hh:mm');
+
+const formatActivityDetailDateTime = (
+  dateTime: string | Date,
+  timeZone: number = 0
+): Date => dayjs(dateTime).utcOffset(timeZone).toDate();
